89d0bab5173f5c738c4c084fe497f641711ccea0
FlorianGit <f.v.kluck@gmail.com>
2018-07-08 20:56:30 +0200

    Add solution for 146, lrucache

---
+++ src/main/scala/com/gitlab/fvkluck/leetcode/146-lrucache.scala
@@ -0,0 +1,75 @@
+import scala.collection.mutable
+
+class LRUCache(_capacity: Int) {
+  case class Element(key: Int, var value: Int, var prev: Element, var next: Element)
+  var head: Element = null
+  var last: Element = null
+  var cache: mutable.Map[Int, Element] = new mutable.HashMap[Int, Element]()
+  var size: Int = 0
+
+  println("")
+  println("new cache")
+
+  private def prepend(elem: Element) = {
+    cache += elem.key -> elem
+    if (head == null) {
+      head = elem
+      last = elem
+      elem.prev = head
+      elem.next = last
+    } else {
+      head.prev = elem
+      elem.next = head
+      head = elem
+      elem.prev = head
+    }
+    size += 1
+  }
+
+  private def remove(elem: Element) = {
+    if (elem != null) {
+      if (head == elem) {
+        head = elem.next
+        elem.next.prev = head
+      } else {
+        elem.prev.next = elem.next
+      }
+      if (last == elem) {
+        last = elem.prev
+        elem.prev.next = last
+      } else {
+        elem.next.prev = elem.prev
+      }
+      elem.next = null
+      elem.prev = null
+      cache -= elem.key
+      size -= 1
+    }
+  }
+
+  def get(key: Int): Int = {
+    var elem = cache.getOrElse(key, null)
+    if (elem != null) {
+      remove(elem)
+      prepend(elem)
+      elem.value
+    } else {
+      -1
+    }
+  }
+
+  def put(key: Int, value: Int) {
+    if (cache contains key) {
+      var elem = cache(key)
+      remove(elem)
+      elem.value = value
+      prepend(elem)
+    } else {
+      var elem = new Element(key, value, null, null)
+      prepend(elem)
+      if (size > _capacity) {
+        remove(last)
+      }
+    }
+  }
+}
---
+++ src/test/scala/com/gitlab/fvkluck/leetcode/146-lrucacheSpec.scala
@@ -0,0 +1,54 @@
+import org.scalatest.FlatSpec
+
+class LruCacheSpec extends FlatSpec {
+  "An LRUCache" should "return value added to it" in {
+    val cache = new LRUCache(1)
+    cache.put(1, 1)
+    assert(cache.get(1) == 1)
+  }
+
+  "An LRUCache" should "return null when requesting unknown value" in {
+    val cache = new LRUCache(1)
+    assert(cache.get(1) == -1)
+  }
+
+  "An LRUCache" should "forget least recently used value above capacity" in {
+    val cache = new LRUCache(2)
+    cache.put(1, 2)
+    cache.put(2, 1)
+    cache.get(1)
+    cache.put(3, 3)
+    assert(cache.get(2) == -1)
+  }
+
+  "An LRUCache" should "enable updating of values" in {
+    val cache = new LRUCache(1)
+    cache.put(1, 1)
+    cache.put(1, 2)
+    assert (cache.get(1) == 2)
+  }
+
+  "An LRUCache" should "pass simple leetcode test case" in {
+    val cache = new LRUCache(2)
+    cache.put(1, 1)
+    cache.put(2, 2)
+    assert(cache.get(1) == 1)
+    cache.put(3, 3)
+    assert(cache.get(2) == -1)
+    cache.put(4, 4)
+    assert(cache.get(1) == -1)
+    assert(cache.get(3) == 3)
+    assert(cache.get(4) == 4)
+  }
+
+  "An LRUCache" should "pass second leetcode test case" in {
+    val cache = new LRUCache(2)
+    cache.put(2, 1)
+    cache.put(1, 1)
+    assert(cache.get(2) == 1)
+    cache.put(4, 1)
+    assert(cache.get(1) == -1)
+    assert(cache.get(2) == 1)
+  }
+
+}
